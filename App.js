a1cf8d91a3066ae0cd36338fe89bb20f2bdfe837
Dan <and.dan19@gmail.com>
2022-11-07 23:38:20 -0300

    To test

--- App.js
+++ App.js
@@ -85,11 +85,11 @@ const App = () => {
     getState();
 
     if (Platform.OS === 'android' && Platform.Version >= 23) {
-      PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION).then((result) => {
+      PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION, PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN).then((result) => {
         if (result) {
           console.log('Permission is OK')
         } else {
-          PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION).then((result) => {
+          PermissionsAndroid.requestMultiple(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION, PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN).then((result) => {
             if (result) {
               console.log('User accept')
             } else {
